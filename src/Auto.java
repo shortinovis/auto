810a250d3ab286d74c153fd04da06d55ee2d22f7
cortinovis.21123 <cortinovis.matteo.studente88@itispaleocapa.it>
2024-09-30 12:19:04 +0200

    creazione attributi

---
+++ src/Auto.java
@@ -0,0 +1,14 @@
+public class Auto {
+    private String modello;
+    private String marca;
+    private double prezzo;
+
+    public Auto (String marc, String mod, double prez){
+        modello=mod;
+        marca=marc;
+        prezzo=prez;
+    }
+    public String getModello{
+
+    }
+}
