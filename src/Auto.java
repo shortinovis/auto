042b5c04a22810cb296a4093c52ed5ed56d257a6
cortinovis.21123 <cortinovis.matteo.studente88@itispaleocapa.it>
2024-09-30 12:24:15 +0200

    creazione dei set e get

--- src/Auto.java
+++ src/Auto.java
@@ -8,7 +8,23 @@ public class Auto {
         marca=marc;
         prezzo=prez;
     }
-    public String getModello{
+    public String getModello() {
+        return modello;
+    }
+    public void setModello(String mod){
+        modello=mod;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marc){
+        marca=marc;
+    }
+    public double getPrezzo() {
+        return prezzo;
+    }
+    public void setPrezzo(double prez){
+        prezzo=prez;
+    }
 
     }
-}
